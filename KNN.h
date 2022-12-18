9c09b3ef11fb2d385b6394202ad954e06fb13956
RozaShatz <rozshatz@gmail.com>
2022-12-18 13:52:32 +0200

    documentation

--- KNN.h
+++ KNN.h
@@ -14,22 +14,42 @@ private:
     int k;
     std::string distanceType;
     std::vector<std::vector<double >> classified;
-    std::vector<double> input;
     std::vector<std::string> types;
-    std::map<int, std::string> numToType;
-    std::map<std::string, int> typeToNum;
+    int numT;
 
-    void typesToNumbers();
-
+/**
+ *function get 2 vectors and name of distance to use and return the distance using the distances.h file.
+ * @param v1 first vector.
+ * @param v2 second vector.
+ * @param distanceType name of distance.
+ * @return the calculated distance.
+ */
     double distance(std::vector<double> v1, std::vector<double> v2, std::string distanceType);
 
-    int runNeighbors();
+/**
+ * the function get input vector, tun over the classified vectors and return the classification of the vector.
+ * @param input vector to classify.
+ * @return classification of the vector.
+ */
+    int runNeighbors(std::vector<double> input);
 
 public:
-    KNN(int kValue, std::string disType, std::vector<double> inputK, std::vector<std::vector<double>> classifiedK,
-        std::vector<std::string> type);
 
-    std::string classify();
+/**
+ * constructor.
+ * @param kValue number of k neighbors.
+ * @param disType type of distance.
+ * @param classifiedK vector of classified vectors, where rhe last parameter is the classification.
+ * @param numType number of possible classifications.
+ */
+    KNN(int kValue, std::string disType, std::vector<std::vector<double>> classifiedK, int numType);
+
+/**
+ * the function get input from the user and calls the runNeighbors function.
+ * @param input the input vector.
+ * @return classification.
+ */
+    int classify(std::vector<double> input);
 
 
 };
