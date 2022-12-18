025829a7b9bc3b47fda2a6af56e829830aa6d8b7
nofgolan <nofigolan@gmail.com>
2022-12-18 13:14:04 +0200

    preprocess

---
+++ PreProcess.h
@@ -0,0 +1,22 @@
+//
+// Created by Nof Golan on 16/12/2022.
+//
+
+#ifndef AP_EX_2_PREPROCESS_H
+#define AP_EX_2_PREPROCESS_H
+
+using namespace std;
+#include "PreProcess.h"
+#include <string>
+#include <vector>
+
+
+bool positiveInteger(const std::string& argv);
+bool distanceMetric(const std::string& argv);
+vector<string> split (string s, string delimiter);
+bool inputCheck(vector<string>& vec);
+std::vector<double> convertStringVectortoDoubleVector(const std::vector<std::string>& stringVector);
+
+
+
+#endif //AP_EX_2_PREPROCESS_H
